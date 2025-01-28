332c073fae8c9bb3269aa4eaa6befd97fc2bec6b
Aadi Kulshrestha <aadi.kulsh@gmail.com>
2025-01-28 16:45:17 -0500

    Fixed mobile issue with projects

--- src/Project.js
+++ src/Project.js
@@ -13,7 +13,7 @@ function Project(props) {
                     <ul className="Tags">{tagItems}</ul>
                     <p>{props.description}</p>
                     <a className="RepoButton" href={props.repository} rel="noreferrer" target="_blank">Visit Repo</a>
-                    {props.demo && <a className="RepoButton" style={{marginLeft: "8vw", marginLeft: "8vw"}} href={props.demo} rel="noreferrer" target="_blank">Demo</a>}
+                    {props.demo && <a className="RepoButton DemoButton" href={props.demo} rel="noreferrer" target="_blank">Demo</a>}
                 </div>
             </div>
         </Fade>
